6e15ce80e19e3c428c0bdbf3a8d93fba79925083
Kirill <45568332+kandelk@users.noreply.github.com>
2020-05-05 14:36:09 +0300

    refactor: change method for multiple usage

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,6 @@
-function showExperience() {
-  var moreText = document.getElementById("more_experience");
-  var btnText = document.getElementById("more_experience_btn");
+function showMore(textId, btnId) {
+  var moreText = document.getElementById(textId);
+  var btnText = document.getElementById(btnId);
 
   if (moreText.style.display === "block") {
     btnText.innerHTML = "Show more";
